b4785fb8d3635162ec4f1b88a401eb272b83c868
Felix Jen <fjen001@nam.ad.pwcinternal.com>
2017-08-07 16:47:27 -0700

    Wlld attempt at appending sections

--- assets/scripts/load-sections.js
+++ assets/scripts/load-sections.js
@@ -11,8 +11,45 @@ $(document).ready(function() {
             data: "view_id=" + view_id,
             success: function(data) {
                 console.log(data);
+                var section_info = JSON.parse(data);
+
+                insertSection(section_info[0], 1);
+
             }
         });
 
     });
+
+    function insertSection(info, num) {
+
+        section_string = "<div class=\"section-block\" id=\"section-" + num + "\"> \
+            <h2>" + info['section-name'] + "</h2> \
+            <p>" + info['section-desc'] + "</p>"
+
+        $("#section-container").append(section_string);
+        var block_id = "#section- " + num;
+
+        for (  var graph in info['graphs'] ) {
+            graph_string = "<div data-netdata=\"" + info['graphs'][graph]['metric-id'] + "\"\
+            data-host=\"" + info['section-host'] + "\"\
+            data-title=\"" + info['graphs'][graph]['title'] + "\" \
+            data-chart-library=\"" + info['graphs'][graph]['library'] + "\"\
+            data-after=\"" + info['graphs'][graph]['after'] + "\"\
+            data-before=\"" + info['graphs'][graph]['before'] + "\"></div>"
+
+            $(block_id).append(graph_string);
+        }
+
+    }
+
+                <div data-netdata="system.io"
+                    data-host="http://registry.my-netdata.io"
+                    data-common-max="io"
+                    data-common-min="io"
+                    data-title="I/O on registry.my-netdata.io"
+                    data-chart-library="dygraph"
+                    data-width="49%"
+                    data-height="100%"
+                    data-after="-300"
+                    ></div>
 });
