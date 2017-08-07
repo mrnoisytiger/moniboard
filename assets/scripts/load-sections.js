cebe558694f0d035d6749254309b999bab810f8b
Felix Jen <fjen001@nam.ad.pwcinternal.com>
2017-08-07 15:36:41 -0700

    Whoops. another typo

--- assets/scripts/load-sections.js
+++ assets/scripts/load-sections.js
@@ -1,7 +1,7 @@
 // Used to load sections
 
 $(document).ready(function() {
-    $("#view-select-list ul").click(function() {
+    $("#view-select-list li").click(function() {
         console.log($(this).data("view-id"));
     });
 });
