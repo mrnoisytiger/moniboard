487715217440cbe72f67eaee18323e34c57dcbcb
Felix Jen <felixjen@Felixs-iMac.local>
2017-08-06 20:55:09 -0700

    This isn't php

--- assets/scripts/get-views.js
+++ assets/scripts/get-views.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     
     var api_loc = "api";
     var views;
-    $.ajax({url: $api_loc/list-views.php, success: function(result) {
+    $.ajax({url: api_loc . "/list-views.php", success: function(result) {
 
         $views = result;        
 
