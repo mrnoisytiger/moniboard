625119836002b5e81e0b5b2543c54245a4c2cb72
Felix Jen <fjen001@nam.ad.pwcinternal.com>
2017-08-07 12:36:12 -0700

    Try to append views

--- assets/scripts/get-views.js
+++ assets/scripts/get-views.js
@@ -3,14 +3,20 @@ $(document).ready(function() {
     
     var api_loc = "api";
     var views;
+    var data;
     $.ajax({
         url: api_loc + "/view/list-views.php", 
         data: {
             format: 'json'
         },
         success: function(result) {
-            alert(result);    
+              data = result;
         },
         type: 'GET', 
     }); 
+
+    data.forEach(function(element) {
+        $list_element = "<li data-id=\"" + element['view-id'] + "\">" + element['view-name'] + "</li>";
+        $("#view-select-list").append($list_element);
+    }, this);
 });
