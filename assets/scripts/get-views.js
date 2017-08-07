f2215e82e867861afc75c8effab7f3c69bbb6b5d
Felix Jen <felixjen@Felixs-iMac.local>
2017-08-06 20:54:22 -0700

    I don't remember js

--- assets/scripts/get-views.js
+++ assets/scripts/get-views.js
@@ -1,10 +1,8 @@
 // Loads the list of available views into the sidebar
-
-var api_loc = "api";
-
 $(document).ready(function() {
     
-    var $views;
+    var api_loc = "api";
+    var views;
     $.ajax({url: $api_loc/list-views.php, success: function(result) {
 
         $views = result;        
