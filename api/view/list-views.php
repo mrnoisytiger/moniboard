434b0035e56126b10d223143b7030786913ddcfd
Felix Jen <fjen001@nam.ad.pwcinternal.com>
2017-08-07 12:20:55 -0700

    No idea what i'm doing

--- api/view/list-views.php
+++ api/view/list-views.php
@@ -21,8 +21,8 @@
 
     $result = $collection->find();
     
-    foreach ($result as $entry => $doc) {
-        echo $entry['_id'], ': ', $doc['view_id'], "\n";
+    foreach ($result as $id => $doc) {
+        echo $id, ': ', $doc['view_id'], "\n";
     }
     //echo json_encode($result);
 
