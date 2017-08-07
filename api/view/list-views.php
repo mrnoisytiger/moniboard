96056a4cac1e4dac132d23460dab2f0b1e73be4f
Felix Jen <fjen001@nam.ad.pwcinternal.com>
2017-08-07 11:31:42 -0700

    Lets try this again

--- api/view/list-views.php
+++ api/view/list-views.php
@@ -1,13 +1,13 @@
 <?php
 
     // Load Libraries
-    require '/api/vendor/autoload.php';
+    require '../vendor/autoload.php';
 
     // Parse configuration ini
     $config = parse_ini_file('../config.ini');
 
     // Connect to MongoDB
-    $db = new MongoClient("mongodb://" . $config['db_host'], array(
+    $db = new MongoDB\Client("mongodb://" . $config['db_host'], array(
         "username" => $config['db_user'],
         "password" => $config['db_pass'],
     ));
