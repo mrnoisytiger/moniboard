14f69d3daf06dcd936e996e37a4ac237d3b60a51
Felix Jen <felixjen@Felixs-iMac.local>
2017-08-07 20:08:19 -0700

    added a div wrapper around graphs and headings

--- assets/scripts/load-sections.js
+++ assets/scripts/load-sections.js
@@ -39,14 +39,14 @@ $(document).ready(function() {
         $("#section-container").append(section_string);
 
         for (  var graph in info['graphs'] ) {
-            graph_string = "<h3>" + info['graphs'][graph]['title'] + "</h3>\
+            graph_string = "<div><h3>" + info['graphs'][graph]['title'] + "</h3>\
             <div data-netdata=\"" + info['graphs'][graph]['metric-id'] + "\"\
             data-host=\"" + info['section-host'] + "\"\
             data-title=\"" + info['graphs'][graph]['title'] + "\" \
             data-chart-library=\"" + info['graphs'][graph]['library'] + "\"\
             data-after=\"" + info['graphs'][graph]['after'] + "\"\
             data-before=\"" + info['graphs'][graph]['before'] + "\"\
-            data-height=\"175px\"></div><br>"
+            data-height=\"175px\"></div></div><br>"
 
             $("#section-" + num).append(graph_string);
         }
