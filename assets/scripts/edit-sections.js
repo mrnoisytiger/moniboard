c6b187b275c8a5b1a0eac3112aa2d5c1cdfcd85f
Felix Jen <fjen001@nam.ad.pwcinternal.com>
2017-08-09 10:34:23 -0700

    Moved from for loop to each function

--- assets/scripts/edit-sections.js
+++ assets/scripts/edit-sections.js
@@ -27,9 +27,9 @@ $(document).ready(function() {
 function getGraphOrder(selected_section) {
     var graph_divs = $(selected_section).find(".section-sortable > div > .netdata-container-with-legend");
     var graph_ids = [];
-    for (var i in graph_divs) {
+    graph_divs.each(function(i) {
         var found_id = graph_divs[i].getAttribute("data-graph-id");
         graph_ids.push(found_id);
-    }
+    });
     console.log(graph_ids);
 }
