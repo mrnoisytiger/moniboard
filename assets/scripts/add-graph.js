42e7d8706381b27ce49ef953f82f5fc7af09ead3
Felix Jen <fjen001@nam.ad.pwcinternal.com>
2017-08-10 13:51:24 -0700

    Added close behavior

--- assets/scripts/add-graph.js
+++ assets/scripts/add-graph.js
@@ -4,7 +4,14 @@ $(document).ready(function() {
     $(document).on('click', '.add-graph-div', function() {
         var active_section = $("this").parent();
 
-        $("#add-overlay").show('300');
-        $("#add-graph").show('300');
+        // Show overlay and add graph form and handle close behavior
+        $("#add-overlay").fadeIn('200');
+        $("#add-graph").fadeIn('200');
+        $(document).on('click', '#add-overlay', function() {
+            // Hide overlay and add graph form
+            $("#add-overlay").fadeOut('200');
+            $("#add-graph").fadeOut('200');
+        });
+        
     })
 })
