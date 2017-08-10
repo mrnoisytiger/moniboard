51f33248ef2f25da2acc77018f175e5c9424ea4e
Felix Jen <fjen001@nam.ad.pwcinternal.com>
2017-08-10 14:32:16 -0700

    try to sort the select element by text alphabetically

--- assets/scripts/add-graph.js
+++ assets/scripts/add-graph.js
@@ -8,11 +8,12 @@ $(document).ready(function() {
             type: "GET",
             url: active_section[0].getAttribute('data-section-host') + "api/v1/charts",
             success: function(data) {
-               console.log(data);
-               for (var i in data['charts']) {
-                   select_option_string = "<option value=\"" + data['charts'][i]['id'] + "\">" + data['charts'][i]['title'] + "</option>";
-                   $("#add-graph-metric").append(select_option_string);
-               }
+                console.log(data);
+                for (var i in data['charts']) {
+                    select_option_string = "<option value=\"" + data['charts'][i]['id'] + "\">" + data['charts'][i]['title'] + "</option>";
+                    $("#add-graph-metric").append(select_option_string);
+                }
+                sortSelect(document.getElementById("add-graph-metric"));
             }
         });
 
@@ -27,3 +28,22 @@ $(document).ready(function() {
         
     })
 })
+
+// Thanks to https://stackoverflow.com/questions/278089/javascript-to-sort-contents-of-select-element (Javascript to sort contents of select element)
+function sortSelect(selElem) {
+    var tmpAry = new Array();
+    for (var i=0;i<selElem.options.length;i++) {
+        tmpAry[i] = new Array();
+        tmpAry[i][0] = selElem.options[i].text;
+        tmpAry[i][1] = selElem.options[i].value;
+    }
+    tmpAry.sort();
+    while (selElem.options.length > 0) {
+        selElem.options[0] = null;
+    }
+    for (var i=0;i<tmpAry.length;i++) {
+        var op = new Option(tmpAry[i][0], tmpAry[i][1]);
+        selElem.options[i] = op;
+    }
+    return;
+}
