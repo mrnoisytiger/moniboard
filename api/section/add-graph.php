9f7cdb7bcf8c9f2dd5eda3195a7d5e0d8cb1f412
Felix Jen <fjen001@nam.ad.pwcinternal.com>
2017-08-10 16:34:21 -0700

    Lets see if this works

--- api/section/add-graph.php
+++ api/section/add-graph.php
@@ -2,4 +2,29 @@
 
     $data = json_decode($_POST['formdata']);
 
+    // Load Libraries
+    require '../vendor/autoload.php';
+
+    // Parse configuration ini
+    $config = parse_ini_file('../config.ini');
+
+    // Connect to MongoDB
+    $db = new MongoDB\Client("mongodb://" . $config['db_host'] . ":" . $config['db_port'], array(
+        "username" => $config['db_user'],
+        "password" => $config['db_pass'],
+        "ssl" => true,
+    ));
+    $collection = $db->$config['db_name']->$config['db_collection'];
+
+    $section_id = $data['section-id'];
+    unset($data['section-id']);
+
+    $collection->updateOne(array( 
+        "section-id" => $section_id,
+        "section-name" => array('$exists'=> true)
+    ), array(
+        '$push' => array(
+            "graphs" => $data
+        ) 
+    ));
 ?>
--- assets/scripts/add-graph.js
+++ assets/scripts/add-graph.js
@@ -34,11 +34,13 @@ $(document).ready(function() {
     $(document).on('click', '#add-graph-submit', function(){
         var add_graph_data = {};
         add_graph_data['section-id'] = active_section[0].getAttribute('data-section-id');
+        add_graph_data['order'] = active_section.find(".section-sortable > div").length;
         add_graph_data['title'] = $("#add-graph-title").val();
-        add_graph_data['metric'] = $("#add-graph-metric").val();
         add_graph_data['after'] = parseInt($("#add-graph-time-after").val());
         add_graph_data['before'] = parseInt($("#add-graph-time-before").val());
-        add_graph_data['order'] = active_section.find(".section-sortable > div").length;
+        add_graph_data['library'] = "dygraph";
+        add_graph_data['metric-id'] = $("#add-graph-metric").val();
+        add_graph_data['graph-id'] = randomString(16);
 
         console.log(add_graph_data);
 
@@ -47,7 +49,6 @@ $(document).ready(function() {
             url: "api/section/add-graph.php",
             data: "formdata=" + JSON.stringify(add_graph_data),
             success: function(data) {
-                location.reload();
             }
         });
     });
@@ -70,4 +71,14 @@ function sortSelect(selElem) {
         selElem.options[i] = op;
     }
     return;
+}
+
+// Thanks to https://www.thepolyglotdeveloper.com/2015/03/create-a-random-nonce-string-using-javascript/ (Creating a random nonce string in JS)
+var randomString = function(length) {
+    var text = "";
+    var possible = "abcdefghijklmnopqrstuvwxyz";
+    for(var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
 }
