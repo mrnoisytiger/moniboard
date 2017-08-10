eb78dbbbf277779f6dee7c278951d53cb79db853
Felix Jen <fjen001@nam.ad.pwcinternal.com>
2017-08-10 13:55:53 -0700

    try to get charts object

--- assets/scripts/add-graph.js
+++ assets/scripts/add-graph.js
@@ -4,6 +4,15 @@ $(document).ready(function() {
     $(document).on('click', '.add-graph-div', function() {
         var active_section = $("this").parent();
 
+        $.ajax({
+            type: "POST",
+            url: active_section.getAttribute('data-section-host') + "api/v1/charts",
+            data: "view_id=" + view_id,
+            success: function(data) {
+               console.log(JSON.parse(data));
+            }
+        });
+
         // Show overlay and add graph form and handle close behavior
         $("#add-overlay").fadeIn('200');
         $("#add-graph").fadeIn('200');
