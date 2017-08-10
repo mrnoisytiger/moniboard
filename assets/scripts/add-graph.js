0c8e6a447a1ba0717fb31be82e6891181f99be21
Felix Jen <fjen001@nam.ad.pwcinternal.com>
2017-08-10 16:12:24 -0700

    Collect the form data into an object

--- assets/scripts/add-graph.js
+++ assets/scripts/add-graph.js
@@ -1,8 +1,10 @@
 // Used to bring up the add graphs form and send to backend
 
 $(document).ready(function() {
+    var active_section;
+
     $(document).on('click', '.add-graph-trigger', function() {
-        var active_section = $(this).parent();
+        active_section = $(this).parent();
 
         $.ajax({
             type: "GET",
@@ -27,9 +29,28 @@ $(document).ready(function() {
             $("#add-overlay").fadeOut('200');
             $("#add-graph").fadeOut('200');
         });
+    });
+    
+    $(document).on('click', '#add-graph-submit', function(){
+        var add_graph_data = {};
+        add_graph_data['section-id'] = active_section[0].getAttribute('data-section-id');
+        add_graph_data['title'] = $("#add-graph-title").val();
+        add_graph_data['metirc'] = $("#add-graph-matric").val();
+        add_graph_data['after'] = $("#add-graph-time-after").val();
+        add_graph_data['before'] = $("#add-graph-time-before").val();
+
+        console.log(add_graph_data);
         
-    })
-})
+        $.ajax({
+            type: "POST",
+            url: "api/section/add_graph.php",
+            data: "formdata=" + JSON.stringify(add_graph_data),
+            success: function(data) {
+                location.reload();
+            }
+        });
+    });
+});
 
 // Thanks to https://stackoverflow.com/questions/278089/javascript-to-sort-contents-of-select-element (Javascript to sort contents of select element)
 function sortSelect(selElem) {
